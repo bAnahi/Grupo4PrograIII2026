c24367b7b0c4b7214e49e0fb109551bca8064c12
Isaac Ventura <eventurag3@miumg.edu.gt>
2026-04-15 23:13:10 -0600

    Clases

---
+++ src/main/java/gt/edu/umg/grupo4prograiii2026/arbolb/NodoB.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gt.edu.umg.grupo4prograiii2026.arbolb;
+
+/**
+ *
+ * @author whiteHat
+ */
+public class NodoB {
+    
+}
